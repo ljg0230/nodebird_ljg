7deb0db3232e63b184a51dac05ed02761708ccbe
ljg0117 <ljg0117@shinsung.co.kr>
2019-09-20 17:28:53 +0900

    start

---
+++ frontend/.next/static/development/pages/profile.js
@@ -0,0 +1,90 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\profile.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fprofile&absolutePagePath=D%3A%5C%EC%9D%B4%EC%A0%95%EA%B1%B8%5Cweb%5Creact_nodebird%5Cfrontend%5Cpages%5Cprofile.js!./":
+/*!*************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fprofile&absolutePagePath=D%3A%5C%EC%9D%B4%EC%A0%95%EA%B1%B8%5Cweb%5Creact_nodebird%5Cfrontend%5Cpages%5Cprofile.js ***!
+  \*************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/profile", function() {
+      var mod = __webpack_require__(/*! ./pages/profile.js */ "./pages/profile.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/profile.js */ "./pages/profile.js", function() {
+          if(!next.router.components["/profile"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/profile.js */ "./pages/profile.js")
+          next.router.update("/profile", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_129a35c7ec57967eb265 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_129a35c7ec57967eb265 */ "dll-reference dll_129a35c7ec57967eb265"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/profile.js":
+/*!**************************!*\
+  !*** ./pages/profile.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "D:\\\uC774\uC815\uAC78\\web\\react_nodebird\\frontend\\pages\\profile.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Profile = function Profile() {
+  return __jsx(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "\uB0B4 \uD504\uB85C\uD544"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Profile);
+
+/***/ }),
+
+/***/ 1:
+/*!*****************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fprofile&absolutePagePath=D%3A%5C%EC%9D%B4%EC%A0%95%EA%B1%B8%5Cweb%5Creact_nodebird%5Cfrontend%5Cpages%5Cprofile.js ***!
+  \*****************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fprofile&absolutePagePath=D%3A%5C%EC%9D%B4%EC%A0%95%EA%B1%B8%5Cweb%5Creact_nodebird%5Cfrontend%5Cpages%5Cprofile.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fprofile&absolutePagePath=D%3A%5C%EC%9D%B4%EC%A0%95%EA%B1%B8%5Cweb%5Creact_nodebird%5Cfrontend%5Cpages%5Cprofile.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_129a35c7ec57967eb265":
+/*!*******************************************!*\
+  !*** external "dll_129a35c7ec57967eb265" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_129a35c7ec57967eb265;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=profile.js.map
---
+++ frontend/pages/signup.js
@@ -0,0 +1,110 @@
+import React, { useState, useCallback } from "react";
+import { Form, Input, Checkbox, Button } from "antd";
+
+const Signup = () => {
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  const useInput = (initValue = nell) => {
+    // 커스텀 hook으로 중복되는 내용을 줄임
+    const [value, setter] = useState(initValue);
+    const handler = useCallback(e => {
+      setter(e.target.value);
+    }, []);
+    return [value, handler];
+  };
+
+  const [id, onChangeId] = useInput("");
+  const [nick, onChangeNick] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      if (password !== passwordCheck) {
+        return setPasswordError(true);
+      }
+      if (!term) {
+        return setTermError(true);
+      }
+    },
+    [password, passwordCheck, term]
+  );
+
+  const onChangePasswordCheck = useCallback(
+    e => {
+      setPasswordError(e.target.value !== password);
+      setPasswordCheck(e.target.value);
+    },
+    [password]
+  );
+
+  const onChangeTerm = useCallback(e => {
+    setTermError(false);
+    setTerm(e.target.checked);
+  }, []);
+
+  return (
+    <>
+      <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <Input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+        <div>
+          <label htmlFor="user-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nick}
+            required
+            onChange={onChangeNick}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-pass">비밀번호</label>
+          <br />
+          <Input
+            name="user-pass"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-pass-chk">비밀번호체크</label>
+          <br />
+          <Input
+            name="user-pass-chk"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
+            열심히 할 거야?
+          </Checkbox>
+          {termError && (
+            <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>
+          )}
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
+    </>
+  );
+};
+
+export default Signup;
